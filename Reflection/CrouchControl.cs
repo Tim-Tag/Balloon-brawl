4170c1e5f9ffd282e299001f48c18c2cac8ec0d1
Tim-Tag <124945331+Tim-Tag@users.noreply.github.com>
2023-11-07 10:58:43 +1100

    Create CrouchControl.cs

---
+++ Reflection/CrouchControl.cs
@@ -0,0 +1,19 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouchcontrol : MonoBehaviour
+{
+
+    public CharacterController PlayerHeight;
+    public float normalHeight, crouchHeight;
+
+    void Update()
+    {
+        if (controls.Player.crouch.triggered()) ;
+        {
+            Debug.Log("crouching");
+            PlayerHeight.height = crouchHeight;
+        }
+    }
+}eeeeeeeeeeeao0itsinthedot:):D */
